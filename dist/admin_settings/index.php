1e23f2f70322110ba25321cbe86b29a77dbacb88
KrashLeviathan <nathankarasch@gmail.com>
2017-04-25 13:51:05 -0500

    Admin settings: survey defaults UI

--- dist/admin_settings/index.php
+++ dist/admin_settings/index.php
@@ -53,6 +53,9 @@ $phone = $user['phone'];
                     <div class="well bs-component">
                         <fieldset id="form-user-fieldset" disabled>
 
+                            <div class="col-lg-12">
+                                <h3>User Settings</h3>
+                            </div>
                             <div class="form-group col-sm-6">
                                 <label for="inputFirstName" class="control-label">First Name</label>
                                 <div>
@@ -87,60 +90,80 @@ $phone = $user['phone'];
                     <div class="well bs-component">
 
                         <fieldset id="form-admin-fieldset" disabled>
-
+                            <div class="col-lg-12">
+                                <h3>Survey Defaults</h3>
+                            </div>
                             <div class="form-group col-sm-4">
-                                <label for="" class="control-label"></label>
+                                <label for="replays_allowed" class="control-label">Replays Allowed</label>
                                 <div>
-                                    <input type="text" class="form-control" name="" id="" value="<?= 'TODO' ?>">
+                                    <input type="number" class="form-control" name="replays_allowed"
+                                           id="replays_allowed" value="<?= 'TODO' ?>">
                                 </div>
                             </div>
                             <div class="form-group col-sm-4">
-                                <label for="" class="control-label"></label>
-                                <div>
-                                    <input type="text" class="form-control" name="" id="" value="<?= 'TODO' ?>">
+                                <label for="survey_time_limit" class="control-label">Survey Time Limit</label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="survey_time_limit"
+                                           id="survey_time_limit" value="<?= 'TODO' ?>">
+                                    <span class="input-group-addon">minutes</span>
                                 </div>
                             </div>
                             <div class="form-group col-sm-4">
-                                <label for="" class="control-label"></label>
-                                <div>
-                                    <input type="text" class="form-control" name="" id="" value="<?= 'TODO' ?>">
+                                <label for="est_length" class="control-label">Estimated Length</label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="est_length"
+                                           id="est_length" value="<?= 'TODO' ?>">
+                                    <span class="input-group-addon">minutes</span>
                                 </div>
                             </div>
                             <div class="form-group col-sm-4">
-                                <label for="" class="control-label"></label>
+                                <label for="notif_enabled" class="control-label">Notifications Enabled</label>
                                 <div>
-                                    <input type="text" class="form-control" name="" id="" value="<?= 'TODO' ?>">
+                                    <div class="radio">
+                                        <label>
+                                            <input type="radio" name="notif_enabled" value="1">
+                                            Yes
+                                        </label>
+                                    </div>
+                                    <div class="radio">
+                                        <label>
+                                            <input type="radio" name="notif_enabled" value="0">
+                                            No
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
                             <div class="form-group col-sm-4">
-                                <label for="" class="control-label"></label>
+                                <label for="notif_email" class="control-label">Notification Email</label>
                                 <div>
-                                    <input type="text" class="form-control" name="" id="" value="<?= 'TODO' ?>">
+                                    <input type="email" class="form-control" name="notif_email"
+                                           id="notif_email" value="<?= 'TODO' ?>">
                                 </div>
                             </div>
                             <div class="form-group col-sm-4">
-                                <label for="" class="control-label"></label>
-                                <div>
-                                    <input type="text" class="form-control" name="" id="" value="<?= 'TODO' ?>">
+                                <label for="rating_threshold" class="control-label">Target Rating Threshold</label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="rating_threshold"
+                                           id="rating_threshold" value="<?= 'TODO' ?>">
+                                    <span class="input-group-addon">ratings per file</span>
                                 </div>
-                            </div>
-
-                        </fieldset>
-
-                        <!-- Submission Buttons -->
-                        <fieldset>
-                            <div class="form-group col-lg-12">
-                                <button id="edit-btn" type="button" class="btn btn-primary" onclick="onEditClicked()">
-                                    Edit
-                                </button>
-                                <button id="cancel-btn" type="reset" class="btn btn-default hidden"
-                                        onclick="onCancelClicked()">Cancel
-                                </button>
-                                <button id="submit-btn" type="submit" class="btn btn-primary hidden">Submit
-                                </button>
                             </div>
                         </fieldset>
                     </div>
+
+                    <!-- Submission Buttons -->
+                    <fieldset>
+                        <div class="form-group col-lg-12">
+                            <button id="edit-btn" type="button" class="btn btn-primary" onclick="onEditClicked()">
+                                Edit
+                            </button>
+                            <button id="cancel-btn" type="reset" class="btn btn-default hidden"
+                                    onclick="onCancelClicked()">Cancel
+                            </button>
+                            <button id="submit-btn" type="submit" class="btn btn-primary hidden">Submit
+                            </button>
+                        </div>
+                    </fieldset>
                 </form>
             </div>
         </div>
