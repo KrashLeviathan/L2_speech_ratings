2a2c778e69ef17a682bfdbd037febeaf485c5a2c
KrashLeviathan <nathankarasch@gmail.com>
2017-04-28 12:43:46 -0500

    Filled out About page

--- dist/about/index.php
+++ dist/about/index.php
@@ -13,6 +13,20 @@
     <div class="bs-docs-section">
         <div class="row">
             <div class="col-lg-12">
+                <p>Thank you for taking the time to participate in my research. I am an applied linguist and assistant
+                    professor of Spanish in the Department of World Languages and Cultures at Iowa State University. In
+                    my research, I study how individuals learn additional languages. In particular, I am interested in
+                    how language learners' speaking ability changes over time.</p>
+                <p>For more information, please visit
+                    <a href="http://language.iastate.edu/portfolio_page/nagle/"
+                       target="_blank">http://language.iastate.edu/portfolio_page/nagle/</a>.
+                </p>
+                <blockquote class="l2sr-mbot-sm l2sr-mtop-sm">
+                    Charles Nagle, PhD<br>
+                    Assistant Professor of Spanish Linguistics<br>
+                    Department of World Languages and Cultures<br>
+                    Iowa State University
+                </blockquote>
             </div>
         </div>
     </div>
